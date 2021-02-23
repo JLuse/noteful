d8bb67918d91a2fb0031dd0cf56b87e374f68df9
Jordan Luse <j.luse7@gmail.com>
2021-02-23 11:37:15 -0800

    Fixed typo

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -4,11 +4,11 @@ export const findFolder = (folders=[], folder_id) =>
 export const findNote = (notes=[], noteId) =>
   notes.find(note => note.id === noteId)
 
-export const getNotesForFolder = (notes=[], folderId) => (
-  (!folderId)
+export const getNotesForFolder = (notes=[], folder_id) => (
+  (!folder_id)
     ? notes
-    : notes.filter(note => note.folder_id === folderId)
+    : notes.filter(note => note.folder_id === folder_id)
 )
 
-export const countNotesForFolder = (notes=[], folderId) =>
+export const countNotesForFolder = (notes=[], folder_id) =>
   notes.filter(note => note.folder_id === folder_id).length
